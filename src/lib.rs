a0a3f0439a2343f821a9c1cd28adc3a4c6a08c06
Mick van Gelderen <mickvangelderen@gmail.com>
2019-11-02 10:41:02 +0100

    Make the types have byte-alignment

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,35 @@
 //! Belene provides little endian and big endian versions of the basic integral
 //! types.
 //!
-//! These types are aligned like their native endian versions. You might want to
-//! use #[repr(packed)] for your type definitions because of it.
+//! The types have byte alignment so they can be used in types that are
+//! transmuted or cast from bytes.
 
 use std::fmt;
 
 macro_rules! impl_types {
     ($(
-        $be: ident, $le: ident, $ne: ident,
+        $be: ident, $le: ident, $ne: ident, [u8; $n: expr],
     )*) => {
         $(
             #[derive(Default, Copy, Clone, Eq, PartialEq, Hash)]
             #[allow(non_camel_case_types)]
             #[repr(transparent)]
-            pub struct $be($ne);
+            pub struct $be(pub [u8; $n]);
 
             impl $be {
                 #[inline]
-                pub const fn from_ne(val: $ne) -> Self {
-                    Self(val.to_be())
+                pub const fn to_bytes(&self) -> [u8; $n] {
+                    self.0
                 }
 
                 #[inline]
-                pub const fn to_ne(self) -> $ne {
-                    $ne::from_be(self.0)
+                pub fn from_ne(val: $ne) -> Self {
+                    Self(val.to_be_bytes())
+                }
+
+                #[inline]
+                pub fn to_ne(self) -> $ne {
+                    $ne::from_be_bytes(self.0)
                 }
             }
 
@@ -37,17 +42,22 @@ macro_rules! impl_types {
             #[derive(Default, Copy, Clone, Eq, PartialEq, Hash)]
             #[allow(non_camel_case_types)]
             #[repr(transparent)]
-            pub struct $le($ne);
+            pub struct $le(pub [u8; $n]);
 
             impl $le {
                 #[inline]
-                pub const fn from_ne(val: $ne) -> Self {
-                    Self(val.to_le())
+                pub const fn to_bytes(&self) -> [u8; $n] {
+                    self.0
                 }
 
                 #[inline]
-                pub const fn to_ne(self) -> $ne {
-                    $ne::from_le(self.0)
+                pub fn from_ne(val: $ne) -> Self {
+                    Self(val.to_le_bytes())
+                }
+
+                #[inline]
+                pub fn to_ne(self) -> $ne {
+                    $ne::from_le_bytes(self.0)
                 }
             }
 
@@ -61,12 +71,12 @@ macro_rules! impl_types {
 }
 
 impl_types!(
-    u16be, u16le, u16,
-    u32be, u32le, u32,
-    u64be, u64le, u64,
-    u128be, u128le, u128,
-    i16be, i16le, i16,
-    i32be, i32le, i32,
-    i64be, i64le, i64,
-    i128be, i128le, i128,
+    u16be, u16le, u16, [u8; 2],
+    u32be, u32le, u32, [u8; 4],
+    u64be, u64le, u64, [u8; 8],
+    u128be, u128le, u128, [u8; 16],
+    i16be, i16le, i16, [u8; 2],
+    i32be, i32le, i32, [u8; 4],
+    i64be, i64le, i64, [u8; 8],
+    i128be, i128le, i128, [u8; 16],
 );
